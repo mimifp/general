57f34d01f41958b3e3b5db51ce8301f36f5a7b53
Miriam Ferreiro Pantín <miriam.ferreirop@gmail.com>
2023-04-04 09:23:02 +0200

    update 3_lambda_functions.py

--- 2_scripts/3_courses/python_intermediate/2_namespaces.py
+++ 2_scripts/3_courses/python_intermediate/2_namespaces.py
@@ -48,3 +48,93 @@ print(' \n -- Local Namespace final -- \n')
 print(locals())
 
 # - enclosing namespace is a namespace created by an enclosing function and any number of enclosed functions inside it. Enclosing namespaces are created specifically when we work with nested functions and just like with the local namespace, will only exist until the function is done executing.
+def calc_paint_amount(width, height):
+  square_feet = width * height
+  
+  def calc_gallons():
+    return square_feet / 400    
+    
+  return calc_gallons()
+    
+print('Number of paint gallons needed: ')
+print(str(calc_paint_amount(30,20)))
+
+
+### Modifying Scope Behavior: nonlocal Statement ###
+walls = [(20, 9), (25, 9), (20, 9), (25, 9)]
+
+
+def calc_paint_amount(wall_measurements):
+
+  square_feet = 0
+
+  def calc_square_feet():
+    
+    for width, height in wall_measurements:
+      nonlocal square_feet
+      square_feet += width * height
+
+  def calc_gallons():
+    return square_feet / 400
+
+  calc_square_feet()
+
+  return calc_gallons()
+
+
+print('Number of paint gallons needed: ')
+print(str(calc_paint_amount(walls)))
+
+
+### Global Scope ### 
+paint_gallons_available = {
+  'red': 50,
+  'blue': 72,
+  'green': 99,
+  'yellow': 33
+}
+
+def print_available(color):
+  print('There are ' + str(paint_gallons_available[color]) + ' gallons available of ' + color + ' paint.')
+
+
+def print_all_colors_available():
+  for color in paint_gallons_available:
+    print(color)
+
+print_available('red')
+print_all_colors_available()
+
+
+### Modifying Scope Behavior: global Statement ###
+def print_available(color):
+  global paint_gallons_available
+  paint_gallons_available = {
+    'red': 50,
+    'blue': 72,
+    'green': 99,
+    'yellow': 33
+  }
+  print('There are ' + str(paint_gallons_available[color]) + ' gallons available of ' + color + ' paint.')
+
+
+print_available('red')
+for color in paint_gallons_available:
+  print(color)
+
+
+### Scope Resolution: The LEGB Rule ###
+color = 'green'
+
+def change_color(new_color):
+  global color
+  to_update = new_color
+  
+  def disp_color():
+    print('The original color was: ' + color)
+
+  disp_color()
+  color = to_update
+  print('The new color is: ' + color)
+
+change_color('blue')
---
+++ 2_scripts/3_courses/python_intermediate/3_lambda_functions.py
@@ -0,0 +1,11 @@
+#######################
+### Lamda functions ###
+#######################
+
+# In Python, a lambda function (also commonly called an anonymous function) is a one-line shorthand for function. 
+# Normal function
+def add_two(my_input):
+  return my_input + 2
+
+# Lamda function
+add_two = lambda my_input: my_input + 2
