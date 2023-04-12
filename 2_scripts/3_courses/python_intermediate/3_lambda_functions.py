2a897aa96228c3d41c5d9f7065a00a45c4062078
Miriam Ferreiro Pantín <miriam.ferreirop@gmail.com>
2023-04-12 14:12:09 +0200

    update 3_lambda_functions.py

--- 2_scripts/3_courses/python_intermediate/3_lambda_functions.py
+++ 2_scripts/3_courses/python_intermediate/3_lambda_functions.py
@@ -77,3 +77,72 @@ box_volume_height15 = make_box_volume_function(15)
 print(box_volume_height15(3,2))
 
 ### MAP function ### 
+doubled = map(lambda input: input*2, int_list)
+ 
+print(list(doubled))
+
+# ------------------------------------------------------------------------------- #
+grade_list = [3.5, 3.7, 2.6, 95, 87]
+
+# Your code below:
+
+# assign the result of your map function to the variable grades_100scale
+
+grades_100scale = map(lambda grade: grade*25 if grade < 4 else grade, grade_list)
+
+# convert grades_100scale to a list and save it as updated_grade_list 
+updated_grade_list = list(grades_100scale)
+
+# print updated_grade_list
+print(updated_grade_list)
+
+### FILTER function ###
+names = ["margarita", "Linda", "Masako", "Maki", "Angela"]
+ 
+M_names = filter(lambda name: name[0] == "M" or name[0] == "m", names) 
+ 
+print(list(M_names))
+
+# ------------------------------------------------------------------------------- #
+
+books = [["Burgess", 1985],
+ ["Orwell", "Nineteen Eighty-four"],
+  ["Murakami", "1Q85"],
+   ["Orwell", 1984],
+    ["Burgess", "Nineteen Eighty-five"],
+     ["Murakami", 1985]]
+
+# Your code below: 
+# assign the result of your filter function to the variable  
+
+string_titles = filter(lambda field: type(field[1]) == str, books)
+
+# convert your filter object to a list stored in the variable 
+string_titles_list = list(string_titles)
+
+# print the list string_titles_list
+print(string_titles_list)
+
+### REDUCE function ###
+from functools import reduce
+ 
+int_list = [3, 6, 9, 12]
+ 
+reduced_int_list = reduce(lambda x,y: x*y, int_list)
+ 
+print(reduced_int_list)
+
+# ------------------------------------------------------------------------------- #
+
+letters = ['r', 'e', 'd', 'u', 'c', 'e']
+
+# your code below:
+
+# remember to import the reduce function
+from functools import reduce 
+
+# store the result of your reduce function in the variable word
+word = reduce(lambda a,b: a+b, letters)
+
+# print word
+print(word)
